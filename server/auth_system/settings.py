a6c6f6f7edbf34493f00636322be11ae2dbf9cec
Christian Del Rosario <ainsley.drosario@gmail.com>
2024-02-06 23:11:42 +0800

    Djoser and server's email setup

--- server/auth_system/settings.py
+++ server/auth_system/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,6 +87,13 @@ DATABASES = {
     }
 }
 
+# Setting up credentials for server's email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv("SERVER_EMAIL")
+EMAIL_HOST_PASSWORD = os.getenv("SERVER_PASSWORD")
+EMAIL_USE_TLS = True
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
